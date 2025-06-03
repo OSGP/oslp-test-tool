433534da4721ef25f9571e472c09c701f75e308e
Jelle Hoffman <jelle.hoffman@alliander.com>
2025-06-03 11:59:28 +0200

    FDP-3204: Adding dependency to protobuf module
    Signed-off-by: IOTFDP <IoTFDP@alliander.com>
    Signed-off-by: Jessie Krul <jessie.krul@alliander.com>

--- protobuf/build.gradle.kts
+++ protobuf/build.gradle.kts
@@ -7,8 +7,19 @@ dependencies {
     implementation(libs.protobufKotlin)
 }
 
+kotlin {
+    jvmToolchain(18)
+}
+
 protobuf {
     protoc {
         artifact = libs.protoc.get().toString()
+    }
+    generateProtoTasks {
+        all().forEach {
+            it.builtins {
+                register("kotlin")
+            }
+        }
     }
 }
